f9edfb6e1ad2573c5a23fb91cd555196e5036877
erdi1974 <e_kalyoncu@hotmail.com>
2022-12-21 14:51:44 +0300

    The solution was not correct when there is a negative number in the array and it was not dynamic. I changed the code completely.

--- src/test/java/com/interview/AllZeroesToTheEnd.java
+++ src/test/java/com/interview/AllZeroesToTheEnd.java
@@ -5,18 +5,37 @@ public class AllZeroesToTheEnd {
 
 //Output = [2, 3, 5, 8, 0, 0, 0]
 
-        int arr[] = {2, 3, 0, 0, 5, 0, 8};
+        int arr[] = {-2,0,0,2, 3, 0, 0, 5, 0, 8,-1,0};
 
-        Arrays.sort(arr);
+        int[] zeroToEndArr = new int[arr.length];
 
-        System.out.println(Arrays.toString(arr));//[0, 0, 0, 2, 3, 5, 8]
-        // index  0  1  2  3  4  5  6
-
-        int reverse[]=new int[arr.length];
-        reverse=Arrays.copyOfRange(arr,3,10);//3-(7+3)
+        for (int i = 0, j=0; i < arr.length ; i++,j++) {
 
 
-        System.out.println(Arrays.toString(reverse));
+
+            if(arr[i]!=0){
+
+                zeroToEndArr[j]=arr[i];
+            }
+            else if(arr[i]==0){
+                j--;
+                continue;
+            }
+
+        }
+        System.out.println(Arrays.toString(zeroToEndArr));
+
+
+//        Arrays.sort(arr);
+//
+//        System.out.println(Arrays.toString(arr));//[0, 0, 0, 2, 3, 5, 8]
+//        // index  0  1  2  3  4  5  6
+//
+//        int reverse[]=new int[arr.length];
+//        reverse=Arrays.copyOfRange(arr,3,10);//3-(7+3)
+//
+//
+//        System.out.println(Arrays.toString(reverse));
 /*
         for (int i = arr.length-1,j=0; i >=0 ; i--,j++) {
            reverse [j] = arr[i];
